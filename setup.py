f3cee17149e1ec6758bd60e24d9e9078cf5ebec6
srimani-programmer <srimani.scholar@gmail.com>
2019-09-10 14:23:03 +0530

    Setup file for the project.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="pysort",
+    version="1.0.0",
+    description="A Powerful package to perform different types of stable and unstable Sorting techniques on the list Data Structure.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/srimani-programmer/pysort",
+    author="Sri Manikanta Palakollu",
+    author_email="srimani.crypter@gmail.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.5",
+        "Intended Audience :: Programmers and Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=["sorting_techniques"],
+    include_package_data=True,
+)
